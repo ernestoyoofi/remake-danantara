97c27f077e6c7327bcd0309f4cc7fc75f8dc658c
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2025-03-10 17:21:28 +0700

    fix text slider

--- app/components/TextSlider.js
+++ app/components/TextSlider.js
@@ -14,20 +14,20 @@ export default function TextSlider({ listString }) {
   
   return <div className="w-full mt-3">
     <div className="relative flex items-center justify-between w-full mx-auto text-center">
-      <button className="flex items-center justify-center w-[22px] h-[190px]" onClick={prevSlide}>
-        <HiChevronLeft size={28}/>
+      <button className="flex items-center justify-center w-[22px] h-[160px] cursor-pointer active:scale-90" onClick={prevSlide}>
+        <HiChevronLeft size={30}/>
       </button>
-      <div className="relative overflow-hidden flex-1 mx-4 w-[calc(100%-44px)] h-[190px]">
+      <div className="relative overflow-hidden flex-1 mx-4 w-[calc(100%-44px)] h-[160px]">
         <div className="flex transition-transform duration-300 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
           {listText.map((text, index) => (
-            <div key={index} className="flex items-start justify-center flex-shrink-0 w-full h-[190px]">
+            <div key={index} className="flex items-start justify-center flex-shrink-0 w-full h-[160px]">
               <div className="text-left">{text}</div>
             </div>
           ))}
         </div>
       </div>
-      <button className="flex items-center justify-center w-[22px] h-[190px]" onClick={nextSlide}>
-        <HiChevronRight size={28}/>
+      <button className="flex items-center justify-center w-[22px] h-[160px] cursor-pointer active:scale-90" onClick={nextSlide}>
+        <HiChevronRight size={30}/>
       </button>
     </div>
   </div>
--- app/content/VisionAndMission.js
+++ app/content/VisionAndMission.js
@@ -3,11 +3,21 @@
 import { RevealWrapper } from  "next-reveal"
 import languageView from "../components/LanguageView"
 import { useLanguage } from "../components/ContextLanguage"
+import { useState } from "react"
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi"
 import TextSlider from "../components/TextSlider"
 
 export default function VisionAndMission() {
   const { language } = useLanguage()
   const displayLang = languageView(language)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev === 0 ? 5 - 1 : prev - 1))
+  }
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev === 5 - 1 ? 0 : prev + 1))
+  }
 
   return <div data-group-element="vision_and_mission">
     <RevealWrapper origin="bottom" reset={false}>
@@ -20,8 +30,8 @@ export default function VisionAndMission() {
         <div className="w-full h-[calc(100vh-170px)] min-h-[490px] max-h-[950px]">
           <div className="absolute w-[calc(100%-calc(16px*0))] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] from-transparent to-white bg-gradient-to-tl z-20 flex justify-center overflow-hidden">
             <div className="w-full max-w-[1873px] max-h-[calc(950px)] flex justify-start">
-              <RevealWrapper className="w-full max-w-[720px] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] px-10 flex justify-center items-center" reset={false} origin="left">
-                <div className="w-[450px] mt-3">
+              <RevealWrapper className="w-full max-w-[720px] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] px-10 flex justify-center items-center" reset={false} origin="right">
+                <div className="w-full max-w-[450px] mt-3">
                   <RevealWrapper origin="left" reset={false} delay={250} distance="160px">
                     <h1 className="text-3xl font-bold bg-red-600 px-4 p-1 pr-6 text-white inline -ml-3">{displayLang("vision")}</h1>
                   </RevealWrapper>
@@ -41,16 +51,16 @@ export default function VisionAndMission() {
       </div>
     </RevealWrapper>
     <RevealWrapper origin="bottom" reset={false}>
-      <div className="w-full h-[calc(100vh-calc(50px*2))] max-h-[940px] min-h-[420px] overflow-hidden mt-6">
+      <div className="w-full h-[calc(100vh-calc(50px*2))] max-h-[940px] min-h-[420px] overflow-hidden">
         <div className="w-full h-[calc(100vh-170px)] min-h-[490px] max-h-[950px]">
-          <div className="absolute w-[calc(100%-calc(16px*0))] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] from-transparent to-white bg-gradient-to-tl z-20 flex justify-center overflow-hidden">
+          <div className="absolute w-[calc(100%-calc(16px*0))] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] from-transparent to-white to-80% bg-gradient-to-br z-20 flex justify-center overflow-hidden">
             <div className="w-full max-w-[1873px] max-h-[calc(950px)] flex justify-end">
-              <RevealWrapper className="w-full max-w-[720px] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] px-10 flex justify-center items-center" reset={false} origin="left">
-                <div className="w-[450px] mt-3">
-                  <RevealWrapper origin="right" reset={false} delay={250} distance="160px" className="px-10 mx-4">
-                    <h1 className="text-3xl font-bold bg-red-600 px-4 p-1 pr-6 text-white inline -mr-3">{displayLang("mission_title")}</h1>
+              <RevealWrapper className="w-full max-w-[720px] h-[calc(100vh-170px)] min-h-[490px] max-h-[950px] px-10 flex justify-center items-center" reset={false} origin="right">
+                <div className="w-full max-w-[450px] mt-3">
+                  <RevealWrapper origin="left" reset={false} delay={250} distance="160px">
+                    <h1 className="text-3xl font-bold bg-red-600 px-4 p-1 pr-6 text-white inline -ml-3">{displayLang("mission_title")}</h1>
                   </RevealWrapper>
-                  <RevealWrapper origin="right" reset={false} delay={450} distance="200px" className="px-10 mx-4">
+                  <RevealWrapper origin="left" reset={false} delay={450} distance="200px">
                     <TextSlider
                       listString={[
                         displayLang("mission_desc_one"),
@@ -68,7 +78,7 @@ export default function VisionAndMission() {
           <img
             alt="Bridge Ilustration"
             className="w-full h-full object-cover"
-            src="/content/bridge-illustration.png"
+            src="/content/landscape-illustration.png"
           />
         </div>
       </div>
